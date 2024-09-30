6745e72d23057fdfb1cc0daa6cd13683f2d06460
Ubuntu <ubuntu@ip-172-31-25-137.ec2.internal>
2024-09-30 04:38:23 +0000

    lab04

--- Lab04/main.c
+++ Lab04/main.c
@@ -3,10 +3,13 @@
 #include <stdio.h>
 
 extern long long int my_mul(long long int a, long long int b);
+extern long long int factorial(long long int n);
 
 int main(void)
 {
     long long int a = my_mul(3, 5);
     printf("Result of my_mul(3, 5) = %ld\n", a);
+    long long int b = factorial(3);
+    printf("Result of factorial(3) = %ld\n", b);
     return 0;
 }
